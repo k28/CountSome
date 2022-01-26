ae923a2fc033c476d2cb24fed99167902f5256e0
Kazuya Hatano <k28@me.com>
2022-01-26 21:52:39 +0900

    Initial Commit

---
+++ CountSome/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CountSome
+//
+//  Created by K.Hatano on 2022/01/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ CountSome/CountSomeApp.swift
@@ -0,0 +1,17 @@
+//
+//  CountSomeApp.swift
+//  CountSome
+//
+//  Created by K.Hatano on 2022/01/26.
+//
+
+import SwiftUI
+
+@main
+struct CountSomeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
